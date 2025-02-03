a4cea574b4e65c9ea2967da5cb213ed3f71f13dc
ncwb <bamboobin@gmail.com>
2025-02-03 19:52:32 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,8 +7,8 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
-sed -i 's/\/bin\/ash/\/bin\/bash/' package/base-files/files/etc/passwd
-sed -i 's/6.6/6.12/g' target/linux/x86/Makefile
+sed -i 's/\/bin\/ash/\/bin\/bash/' openwrt/package/base-files/files/etc/passwd
+sed -i 's/6.6/6.12/g' openwrt/target/linux/x86/Makefile
 #2. Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
