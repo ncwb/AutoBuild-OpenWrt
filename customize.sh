780ad7b7dea407ce7fb28e5b48891859f98446d0
ncwb <bamboobin@gmail.com>
2025-02-08 13:47:52 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -9,11 +9,11 @@
 sed -i 's/192.168.1.1/10.0.0.1/g' openwrt/package/base-files/files/bin/config_generate
 sed -i 's/\/bin\/ash/\/bin\/bash/' openwrt/package/base-files/files/etc/passwd
 sed -i 's/6.6/6.12/g' openwrt/target/linux/x86/Makefile
+sed -i 's/6.6/6.12/g' openwrt/target/linux/x86/Makefile
 #2. Clear the login password
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i 's/CPU_CFLAGS = -Os -pipe/CPU_CFLAGS = -O3 -mtune=generic -pipe/g' openwrt/include/target.mk
 #3. Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf
 mkdir -p openwrt/files/www
 curl -so openwrt/files/www/gdyey.html https://raw.githubusercontent.com/ncwb/AutoBuild-OpenWrt/master/gdyey.html
-curl -s https://raw.githubusercontent.com/ncwb/openwrt-x86-64/openwrt/patch/target-modify_for_x86_64.patch | patch -p1
 #mv gdyey.html openwrt/files/www/gdyey.html
