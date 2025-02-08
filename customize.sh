654040bc0cb2e1ed75aac9f7bf756317b511136b
ncwb <bamboobin@gmail.com>
2025-02-08 19:03:53 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -10,7 +10,7 @@ sed -i 's/192.168.1.1/10.0.0.1/g' openwrt/package/base-files/files/bin/config_ge
 sed -i 's/\/bin\/ash/\/bin\/bash/' openwrt/package/base-files/files/etc/passwd
 sed -i 's/6.6/6.12/g' openwrt/target/linux/x86/Makefile
 #2. Clear the login password
-#sed -i 's/CPU_CFLAGS = -Os -pipe/CPU_CFLAGS = -O3 -mtune=generic -pipe/g' openwrt/include/target.mk
+sed -i 's/CPU_CFLAGS = -Os -pipe/CPU_CFLAGS = -O3 -mtune=generic -pipe/g' openwrt/include/target.mk
 #3. Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf
 mkdir -p openwrt/files/www
