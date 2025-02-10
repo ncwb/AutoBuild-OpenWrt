c402276f016af5c9598ad520ebe0fccdd73ad87c
ncwb <bamboobin@gmail.com>
2025-02-10 09:05:20 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -12,9 +12,8 @@ sed -i 's/6.6/6.12/g' openwrt/target/linux/x86/Makefile
 #2. Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 sed -i 's/CPU_CFLAGS = -Os -pipe/CPU_CFLAGS = -O3 -mtune=generic -pipe/g' openwrt/include/target.mk
-sed -i 's/= .12/= .13/g' openwrt/include/target.mk
-sed -i 's/.12 = e98942d17ef7063b3f2d6d7692bf24899e2e021cf832d19b55308ec8e8e08eff/.13 = f3ebdeea9e555b4cface44e29670056f4024541e6bd222fbcf776c818974fbba/g' openwrt/include/target.mk
-.12 = e98942d17ef7063b3f2d6d7692bf24899e2e021cf832d19b55308ec8e8e08eff
+sed -i 's/= .12/= .13/g' openwrt/include/kernel-6.12
+sed -i 's/.12 = e98942d17ef7063b3f2d6d7692bf24899e2e021cf832d19b55308ec8e8e08eff/.13 = f3ebdeea9e555b4cface44e29670056f4024541e6bd222fbcf776c818974fbba/g' openwrt/include/kernel-6.12
 #3. Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf
 mkdir -p openwrt/files/www
