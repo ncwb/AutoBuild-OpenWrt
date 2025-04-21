1304a35e3c28ee00a018d0319a9c56e5694f2ebc
ncwb <bamboobin@gmail.com>
2025-04-21 10:37:21 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -14,8 +14,6 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-se
 sed -i 's/CPU_CFLAGS = -Os -pipe/CPU_CFLAGS = -O3 -mtune=generic -pipe/g' openwrt/include/target.mk
 sed -i 's/= .23/= .24/g' openwrt/include/kernel-6.12
 sed -i 's/.23 = d8d95404f8deeb7ff6992c0df855025062e9e8182bca6daa27ef2e9275d27749/.24 = 643142c1b5991560dd12f950825cc19e4497b95b82641918ecff1177f4130c1d/g' openwrt/include/kernel-6.12
-sed -i 's/8.7.0/8.8.12/g' openwrt/feeds/packages/net/uugamebooster/Makefile
-sed -i 's/87c72206bfe1c42ab89d6f5083e5c1cce1e5b6d2a191dea07532978bc3847169/a62270c3ed9e264c4de42cf7bfbfa4a28428a6e65455eedbe96c3223ca05e1d0/g' openwrt/feeds/packages/net/uugamebooster/Makefile
 #3. Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf
 mkdir -p openwrt/files/www
