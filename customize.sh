8f699416c6c63b0fda22a37867e71dffd3e72edd
ncwb <bamboobin@gmail.com>
2025-05-26 12:40:50 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -8,12 +8,12 @@
 #1. Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' openwrt/package/base-files/files/bin/config_generate
 sed -i 's/\/bin\/ash/\/bin\/bash/' openwrt/package/base-files/files/etc/passwd
-sed -i 's/6.6/6.12/g' openwrt/target/linux/x86/Makefile
+# sed -i 's/6.6/6.12/g' openwrt/target/linux/x86/Makefile
 #2. Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 sed -i 's/CPU_CFLAGS = -Os -pipe/CPU_CFLAGS = -O3 -mtune=generic -pipe/g' openwrt/include/target.mk
-sed -i 's/= .29/= .30/g' openwrt/include/kernel-6.12
-sed -i 's/.29 = e8b2ec7e2338ccb9c86de7154f6edcaadfce80907493c143e85a82776bb5064d/.30 = df046a48971e40ce0b2e003e7e55b6b1e7da2912120eb216d5d6c8450c9cf82e/g' openwrt/include/kernel-6.12
+# sed -i 's/= .29/= .30/g' openwrt/include/kernel-6.12
+# sed -i 's/.29 = e8b2ec7e2338ccb9c86de7154f6edcaadfce80907493c143e85a82776bb5064d/.30 = df046a48971e40ce0b2e003e7e55b6b1e7da2912120eb216d5d6c8450c9cf82e/g' openwrt/include/kernel-6.12
 # sed -i '100i 		--enable-threads=posix \' openwrt/toolchain/gcc/common.mk
 #3. Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf
