e64ac5cc4aa509d32cabc176b8d26da69c736c1e
ncwb <bamboobin@gmail.com>
2026-04-10 13:00:28 +0800

    Comment out CPU_CFLAGS sed command
    Comment out CPU_CFLAGS modification in customize.sh

--- customize.sh
+++ customize.sh
@@ -11,7 +11,7 @@ sed -i 's/\/bin\/ash/\/bin\/bash/' openwrt/package/base-files/files/etc/passwd
 #sed -i 's/6.12/6.18/g' openwrt/target/linux/x86/Makefile
 #2. Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
-sed -i 's/CPU_CFLAGS = -Os -pipe/CPU_CFLAGS = -O3 -mtune=generic -pipe/g' openwrt/include/target.mk
+#sed -i 's/CPU_CFLAGS = -Os -pipe/CPU_CFLAGS = -O3 -mtune=generic -pipe/g' openwrt/include/target.mk
 # sed -i 's/= .63/= .68/g' openwrt/include/kernel-6.12
 # sed -i 's/.63 = 9502c5ffe4b894383c97abfccf74430a84732f04ee476b9c0d87635b29df7db3/.68 = d367c7504bd4da520dd01eb08125d2d0ac088bc8af4cd56d236f2074dd4225b7/g' openwrt/include/kernel-6.12
 # sed -i '100i 		--enable-threads=posix \' openwrt/toolchain/gcc/common.mk
